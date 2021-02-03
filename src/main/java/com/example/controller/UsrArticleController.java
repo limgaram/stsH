e6ccecbb6002716ef65c98b288ed4a89e4c4e864
limgaram <garam990317@naver.com>
2021-02-03 23:52:46 +0900

    updateDate 추

--- src/main/java/com/example/controller/UsrArticleController.java
+++ src/main/java/com/example/controller/UsrArticleController.java
@@ -12,107 +12,106 @@ import com.example.dto.Article;
 import com.example.util.Util;
 
 public class UsrArticleController {
-	
+
 	private List<Article> articles;
 	private int articleLastId;
-	
+
 	public UsrArticleController() {
 		articles = new ArrayList<>();
 		articleLastId = 0;
-		
-		//+앞에 => 아이디에 1이 더해져서 들어감
-		articles.add(new Article(++articleLastId, "2020-12-12 12:12:12", "제목1", "내용1"));
-		articles.add(new Article(++articleLastId, "2020-12-12 12:12:12", "제목2", "내용2"));
+
+		// +앞에 => 아이디에 1이 더해져서 들어감
+		articles.add(new Article(++articleLastId, "2020-12-12 12:12:12", "2020-12-12 12:12:12", "제목1", "내용1"));
+		articles.add(new Article(++articleLastId, "2020-12-12 12:12:12", "2020-12-12 12:12:12", "제목2", "내용2"));
 	}
-	
+
 	@RequestMapping("usr/article/detail")
 	@ResponseBody
 	public Article showDetail(int id) {
 		return articles.get(id - 1);
 	}
-	
+
 	@RequestMapping("usr/article/list")
 	@ResponseBody
 	public List<Article> showList() {
 		return articles;
 	}
-	
+
 	@RequestMapping("usr/article/doAdd")
 	@ResponseBody
 	public Map<String, Object> doAdd(String title, String body) {
-		
+
 		String regDate = Util.getNowDateStr();
-		articles.add(new Article(++articleLastId, regDate, title, body));
-		
+		String updateDate = regDate;
+		articles.add(new Article(++articleLastId, regDate, updateDate, title, body));
+
 		Map<String, Object> rs = new HashMap<>();
 		rs.put("resultCode", "S-1");
 		rs.put("msg", "성공하였습니다.");
-		//articleLastId(+1된 id)를 추가된 게시물 번호로 씀
+		// articleLastId(+1된 id)를 추가된 게시물 번호로 씀
 		rs.put("id", articleLastId);
-		
+
 		return rs;
 	}
-	
+
 	@RequestMapping("usr/article/doDelete")
 	@ResponseBody
 	public Map<String, Object> doDelete(int id) {
 		boolean deleteArticleRs = deleteArticle(id);
-		
+
 		Map<String, Object> rs = new HashMap<>();
-		
-		if(deleteArticleRs) {
+
+		if (deleteArticleRs) {
 			rs.put("resultCode", "S-1");
-			rs.put("msg", "성공하였습니다.");		
+			rs.put("msg", "성공하였습니다.");
 		} else {
 			rs.put("resultCode", "F-1");
 			rs.put("msg", "해당 게시물은 존재하지 않습니다.");
 		}
-		//받아온 id가 삭제되는 id
+		// 받아온 id가 삭제되는 id
 		rs.put("id", id);
-		
+
 		return rs;
 	}
 
 	private boolean deleteArticle(int id) {
-		for(Article article : articles) {
-			if(article.getId() == id) {
+		for (Article article : articles) {
+			if (article.getId() == id) {
 				articles.remove(article);
 				return true;
 			}
 		}
 		return false;
 	}
-	
+
 	@RequestMapping("usr/article/doModify")
 	@ResponseBody
 	public Map<String, Object> doModify(int id, String title, String body) {
 		Article selArticle = null;
-		
-		for(Article article : articles) {
-			if(article.getId() == id) {
+
+		for (Article article : articles) {
+			if (article.getId() == id) {
 				selArticle = article;
 				break;
 			}
 		}
-		
+
 		Map<String, Object> rs = new HashMap<>();
-		
-		if(selArticle == null) {
+
+		if (selArticle == null) {
 			rs.put("resultCode", "F-1");
 			rs.put("msg", String.format("%d번 게시물은 존재하지 않습니다.", id));
 		}
-		
+
+		selArticle.setUpdateDate(Util.getNowDateStr());
 		selArticle.setTitle(title);
 		selArticle.setBody(body);
-		
+
 		rs.put("resultCode", "S-1");
 		rs.put("msg", String.format("%d번 게시물이 수정되었습니다.", id));
 		rs.put("id", id);
-		
+
 		return rs;
 	}
-	
-	
-	
 
 }
--- src/main/java/com/example/dto/Article.java
+++ src/main/java/com/example/dto/Article.java
@@ -3,20 +3,23 @@ package com.example.dto;
 public class Article {
 	private int id;
 	private String regDate;
+	private String updateDate;
 	private String title;
 	private String body;
-	
-	public Article(int id, String regDate, String title, String body) {
+
+	public Article(int id, String regDate, String updateDate, String title, String body) {
 		super();
 		this.id = id;
 		this.regDate = regDate;
+		this.updateDate = updateDate;
 		this.title = title;
 		this.body = body;
 	}
 
 	@Override
 	public String toString() {
-		return "Article [id=" + id + ", regDate=" + regDate + ", title=" + title + ", body=" + body + "]";
+		return "Article [id=" + id + ", regDate=" + regDate + ", updateDate=" + updateDate + ", title=" + title
+				+ ", body=" + body + "]";
 	}
 
 	public int getId() {
@@ -50,9 +53,13 @@ public class Article {
 	public void setBody(String body) {
 		this.body = body;
 	}
-	
-	
-	
-	
+
+	public String getUpdateDate() {
+		return updateDate;
+	}
+
+	public void setUpdateDate(String updateDate) {
+		this.updateDate = updateDate;
+	}
 
 }
