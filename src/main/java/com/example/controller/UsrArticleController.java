56ca9f0423bb1e2ecc55b5e44568e44247243414
limgaram <garam990317@naver.com>
2021-02-05 21:40:49 +0900

    articleService 도입

--- src/main/java/com/example/controller/UsrArticleController.java
+++ src/main/java/com/example/controller/UsrArticleController.java
@@ -5,100 +5,74 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.dto.Article;
 import com.example.dto.ResultData;
+import com.example.service.ArticleService;
 import com.example.util.Util;
 
 public class UsrArticleController {
 
+	@Autowired
+	private ArticleService articleService;
+
 	private List<Article> articles;
 	private int articleLastId;
-
-	public UsrArticleController() {
-		articles = new ArrayList<>();
-		articleLastId = 0;
-
-		// +앞에 => 아이디에 1이 더해져서 들어감
-		articles.add(new Article(++articleLastId, "2020-12-12 12:12:12", "2020-12-12 12:12:12", "제목1", "내용1"));
-		articles.add(new Article(++articleLastId, "2020-12-12 12:12:12", "2020-12-12 12:12:12", "제목2", "내용2"));
-	}
 
 	@RequestMapping("usr/article/detail")
 	@ResponseBody
 	public Article showDetail(int id) {
-		return articles.get(id - 1);
+
+		Article article = articleService.getArticle(id);
+		return article;
 	}
 
 	@RequestMapping("usr/article/list")
 	@ResponseBody
 	public List<Article> showList() {
-		return articles;
+		return articleService.getArticles();
 	}
 
 	@RequestMapping("usr/article/doAdd")
 	@ResponseBody
 	public ResultData doAdd(String title, String body) {
 
-		String regDate = Util.getNowDateStr();
-		String updateDate = regDate;
-		articles.add(new Article(++articleLastId, regDate, updateDate, title, body));
+		if (title == null) {
+			return new ResultData("F-1", "title을 입력해주세요.");
+		}
+		if (body == null) {
+			return new ResultData("F-1", "body를 입력해주세요.");
+		}
 
-		// articleLastId(+1된 id)를 추가된 게시물 번호로 씀
-
-		return new ResultData("S-1", "성공하였습니다.", articleLastId);
+		return articleService.add(title, body);
 	}
 
 	@RequestMapping("usr/article/doDelete")
 	@ResponseBody
 	public ResultData doDelete(int id) {
-		boolean deleteArticleRs = deleteArticle(id);
+		Article article = articleService.getArticle(id);
 
-		Map<String, Object> rs = new HashMap<>();
-
-		if (deleteArticleRs == false) {
+		if (article == null) {
 			return new ResultData("F-1", "해당 게시물은 존재하지 않습니다.");
 		}
-		// 받아온 id가 삭제되는 id
 
-		return new ResultData("S-1", "성공하였습니다.", id);
-	}
-
-	private boolean deleteArticle(int id) {
-		for (Article article : articles) {
-			if (article.getId() == id) {
-				articles.remove(article);
-				return true;
-			}
-		}
-		return false;
+		return articleService.deleteArticle(id);
 	}
 
 	@RequestMapping("usr/article/doModify")
 	@ResponseBody
 	public ResultData doModify(int id, String title, String body) {
-		Article selArticle = null;
 
-		for (Article article : articles) {
-			if (article.getId() == id) {
-				selArticle = article;
-				break;
-			}
+		Article article = articleService.getArticle(id);
+
+		if (article == null) {
+			return new ResultData("F-1", "해당 게시물은 존재하지 않습니다.");
 		}
 
-		Map<String, Object> rs = new HashMap<>();
-
-		if (selArticle == null) {
-			return new ResultData("F-1", String.format("%d번 게시물은 존재하지 않습니다.", id));
-		}
-
-		selArticle.setUpdateDate(Util.getNowDateStr());
-		selArticle.setTitle(title);
-		selArticle.setBody(body);
-
-		return new ResultData("S-1", String.format("%d번 게시물이 수정되었습니다.", id), id);
+		return articleService.modify(id, title, body);
 	}
 
 }
