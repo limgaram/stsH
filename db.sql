74db4136a61ae0530818406b64cc91f6b6557b19
limgaram <garam990317@naver.com>
2021-02-14 20:05:35 +0900

    mybatis 도입

---
+++ db.sql
@@ -0,0 +1,80 @@
+
+GRANT ALL PRIVILEGES ON *.* TO sbsst@`%` IDENTIFIED BY 'sbs123414';
+
+
+
+
+#데이터베이스 생성
+DROP DATABASE IF EXISTS untact;
+CREATE DATABASE untact;
+USE untact;
+
+# 게시물 테이블 생성
+CREATE TABLE article (
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    title CHAR(100) NOT NULL,
+    `body` TEXT NOT NULL
+);
+# 게시물, 테스트 데이터 생성
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = "제목1 입니다.",
+`body` = "내용1 입니다.";
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = "제목2 입니다.",
+`body` = "내용2 입니다.";
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = "제목3 입니다.",
+`body` = "내용3 입니다.";
+
+
+# 회원 테이블 생성
+CREATE TABLE `member` (
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    loginId CHAR(30) NOT NULL,
+    loginPw VARCHAR(100) NOT NULL,
+    `name` CHAR(30) NOT NULL,
+    `nickname` CHAR(30) NOT NULL,
+    `email` CHAR(100) NOT NULL,
+    `cellphoneNo` CHAR(20) NOT NULL
+);
+
+# 로그인 ID로 검색했을 때
+ALTER TABLE `member` ADD UNIQUE INDEX (`loginId`);
+
+# 회원, 테스트 데이터 생성
+INSERT INTO `member`
+SET regDate = NOW(),
+updateDate = NOW(),
+loginId = "user1",
+loginPw = "user1",
+`name` = "user1",
+nickname = "user1",
+cellphoneNo = "01012341234",
+email = "jangka512@gmail.com";
+
+# 게시물 테이블에 회원번호 칼럼 추가
+ALTER TABLE article ADD COLUMN memberId INT(10) UNSIGNED NOT NULL AFTER updateDate;
+
+SELECT * FROM article;
+
+DESC article;
+
+#기존 게시물의 작성자를 회원1로 지정
+UPDATE article
+SET memberId = 1
+WHERE memberId = 0;
+
+
+
--- src/main/java/com/example/dao/ArticleDao.java
+++ src/main/java/com/example/dao/ArticleDao.java
@@ -1,97 +1,24 @@
 package com.example.dao;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.stereotype.Component;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.example.dto.Article;
-import com.example.util.Util;
 
-@Component
-public class ArticleDao {
-	private int articlesLastId;
-	private List<Article> articles;
+@Mapper
+public interface ArticleDao {
 
-	public ArticleDao() {
-		articlesLastId = 0;
-		articles = new ArrayList<>();
+	public Article getArticle(@Param("id") int id);
 
-		// +앞에 => 아이디에 1이 더해져서 들어감
-		articles.add(new Article(++articlesLastId, "2020-12-12 12:12:12", "2020-12-12 12:12:12", "제목1", "내용1"));
-		articles.add(new Article(++articlesLastId, "2020-12-12 12:12:12", "2020-12-12 12:12:12", "제목2", "내용2"));
-	}
+	public List<Article> getArticles(@Param("searchKeywordType") String searchKeywordType,
+			@Param("searchKeyword") String searchKeyword);
 
-	// 찾는 게시글 하나만 가져오기
-	public Article getArticle(int id) {
-		for (Article article : articles) {
-			if (article.getId() == id) {
-				return article;
-			}
-		}
-		return null;
-	}
+	public void addArticle(@Param("title") String title, @Param("body") String body);
 
-	// 전체 게시글 목록 가져오기(검색했으면 검색 게시글 목록 가져오기)
-	public List<Article> getArticles(String searchkeywordType, String searchKeyword) {
+	public void deleteArticle(@Param("id") int id);
 
-		// 검색을 안했으면 전체 게시글 가져오기
-		if (searchKeyword == null) {
-			return articles;
-		}
-
-		// 검색된 게시글들 목록
-		List<Article> filtered = new ArrayList<>();
-
-		for (Article article : articles) {
-			boolean contains = false;
-
-			if (searchkeywordType.equals("title")) {
-				contains = article.getTitle().contains(searchKeyword);
-			} else if (searchkeywordType.equals("body")) {
-				contains = article.getBody().contains(searchKeyword);
-			} else {
-				contains = article.getTitle().contains(searchKeyword);
-
-				if (contains == false) {
-					contains = article.getBody().contains(searchKeyword);
-				}
-			}
-
-			if (contains) {
-				filtered.add(article);
-			}
-		}
-		return filtered;
-
-	}
-
-	public int addArticle(String title, String body) {
-		int id = ++articlesLastId;
-		String regDate = Util.getNowDateStr();
-		String updateDate = regDate;
-
-		articles.add(new Article(id, regDate, updateDate, title, body));
-
-		return id;
-	}
-
-	public boolean deleteArticle(int id) {
-		for (Article article : articles) {
-			if (article.getId() == id) {
-				articles.remove(article);
-				return true;
-			}
-		}
-		return false;
-	}
-
-	public void modifyArticle(int id, String title, String body) {
-		Article article = getArticle(id);
-
-		article.setTitle(title);
-		article.setBody(body);
-		article.setUpdateDate(Util.getNowDateStr());
-	}
+	public void modifyArticle(@Param("id") int id, @Param("title") String title, @Param("body") String body);
 
 }
