473fae192333d69def22a1a988317bd696208c75
limgaram <garam990317@naver.com>
2021-02-17 21:17:37 +0900

    login

--- src/main/java/com/example/controller/UsrMemberController.java
+++ src/main/java/com/example/controller/UsrMemberController.java
@@ -2,6 +2,8 @@ package com.example.controller;
 
 import java.util.Map;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.example.dto.Member;
 import com.example.dto.ResultData;
 import com.example.service.MemberService;
+import com.sun.net.httpserver.Authenticator.Result;
 
 @Controller
 public class UsrMemberController {
@@ -51,5 +54,39 @@ public class UsrMemberController {
 		}
 
 		return memberService.join(param);
+	}
+
+	@RequestMapping("/usr/member/doLogin")
+	@ResponseBody
+	public ResultData doLogin(String loginId, String loginPw, HttpSession session) {
+
+		// 로그인 아이디가 비어있지 않을 때(=로그인이 되어있을 때)
+		if (session.getAttribute("loginedMemberId") != null) {
+			return new ResultData("F-4", "이미 로그인 되었습니다.");
+		}
+
+		if (loginId == null) {
+			return new ResultData("F-1", "loginId를 입력해주세요.");
+		}
+
+		Member existingMember = memberService.getMemberByLoginId(loginId);
+
+		// 기존 회원 아이디에서 못찾은 경우
+		if (existingMember == null) {
+			return new ResultData("F-2", "존재하지 않는 로그인 아이디입니다.", "loginId", loginId);
+		}
+
+		if (loginPw == null) {
+			return new ResultData("F-1", "loginPw를 입력해주세요.");
+		}
+
+		if (existingMember.getLoginPw().equals(loginPw) == false) {
+			return new ResultData("F-3", "비밀번호가 일치하지 않습니다.");
+		}
+
+		// session에 회원 아이디 정보 입력
+		session.setAttribute("loginedMemberId", existingMember.getId());
+
+		return new ResultData("S-1", String.format("%s님 환영합니다.", existingMember.getNickname()));
 	}
 }
